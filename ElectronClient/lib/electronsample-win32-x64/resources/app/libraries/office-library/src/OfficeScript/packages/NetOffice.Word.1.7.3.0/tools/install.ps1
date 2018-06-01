b1576373f0f0bbef9a2c054aa7fac733fdc0f612
Dion Haneveld <dionhaneveld@Dions-MacBook-Pro.local>
2018-06-01 15:35:34 +0200

    removed application sample

--- ElectronClient/lib/electronsample-win32-x64/resources/app/libraries/office-library/src/OfficeScript/packages/NetOffice.Word.1.7.3.0/tools/install.ps1
+++
@@ -1,7 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$ref = $project.Object.References.Item("WordApi")
-if ($ref -and $ref.EmbedInteropTypes)
-{
-    $ref.EmbedInteropTypes = $false
-}
--- ElectronClient/lib/electronsample-win32-x64/resources/app/src/libraries/office-library/index.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  Presentation: require('./lib/powerpoint/presentation'),
-  word: require('./lib/word/application'),
-  excel: require('./lib/excel/application'),
-  powerpoint: require('./lib/powerpoint/application')
-}
