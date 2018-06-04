1b1b384ed2512894b352f6c8cce0357ca06011ac
Dion Haneveld <dhaneveld@servoy.com>
2018-06-04 13:51:03 +0200

    update

--- windows/managers/office/wordManager.js
+++ windows/managers/office/wordManager.js
@@ -1,9 +1,9 @@
 var path = require('path');
 var word = require('./../../../libraries/office-library/index').word;
 
-  //Create a new instance of PowerPoint and try to open the presentation
+  //Create a new instance of Word and try to open the document
 function open(path){
-  excel.open(path, function(err, document) {
+  word.open(path, function(err, document) {
     if(err) throw err;
     console.log('Word path:', document.attr({name:'Path'}, true));
   });
